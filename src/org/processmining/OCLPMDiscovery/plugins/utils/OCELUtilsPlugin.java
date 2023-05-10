20da73fff1cee2e948df1a4a960dcb501ea600a1
Marvin Porsil <marvin.porsil@rwth-aachen.de>
2023-05-10 09:51:46 +0200

    Added dummy flattenening plugin.

--- src/org/processmining/OCLPMDiscovery/plugins/utils/OCELUtilsPlugin.java
+++ src/org/processmining/OCLPMDiscovery/plugins/utils/OCELUtilsPlugin.java
@@ -1,16 +1,19 @@
-package org.processmining.OCLPMDiscovery.utils;
+package org.processmining.OCLPMDiscovery.plugins.utils;
 
+import org.deckfour.xes.model.XLog;
+import org.processmining.OCLPMDiscovery.utils.OCELUtils;
 import org.processmining.contexts.uitopia.annotations.UITopiaVariant;
 import org.processmining.framework.plugin.PluginContext;
 import org.processmining.framework.plugin.annotations.Plugin;
 import org.processmining.framework.plugin.annotations.PluginVariant;
+import org.processmining.ocel.flattening.Flattening;
 import org.processmining.ocel.ocelobjects.OcelEventLog;
 
 @Plugin(
 		name = "OCEL helper methods from the OCLPMs package.", // not shown anywhere anymore because overwritten by uiLabel?
 		parameterLabels = {"OCEL"},
-		returnLabels = { "OCEL" },
-		returnTypes = { OcelEventLog.class },
+		returnLabels = { "OCEL", "Flattened Log" },
+		returnTypes = { OcelEventLog.class, XLog.class },
 		help = "OCEL helper methods from the OCLPMs package."
 )
 public class OCELUtilsPlugin {
@@ -19,13 +22,16 @@ public class OCELUtilsPlugin {
 			affiliation = "RWTH - PADS",
 			author = "Marvin Porsil",
 			email = "marvin.porsil@rwth-aachen.de",
-			uiLabel = "OCEL add dummy case notion" // name shown in ProM
+			uiLabel = "OCEL add dummy case notion and flatten on it." // name shown in ProM
 	)
 	@PluginVariant(
-			variantLabel = "OCEL add dummy case notion",
+			variantLabel = "OCEL add dummy case notion and flatten on it.",
 			requiredParameterLabels = {0}
 	)
-	public static OcelEventLog addDummyCaseNotion(PluginContext context, OcelEventLog ocel) {
-		return OCELUtils.addDummyCaseNotion(ocel, "DummyType", "DummyID");
+	public static Object[] addDummyCaseNotion(PluginContext context, OcelEventLog ocel) {
+		String dummyType = "DummyType";
+		OcelEventLog newocel = OCELUtils.addDummyCaseNotion(ocel, dummyType, "DummyID");
+		XLog flatLog = Flattening.flatten(newocel, dummyType);
+		return new Object[] {newocel, flatLog};
 	}
 }
