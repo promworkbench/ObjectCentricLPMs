432b30b070c1b7ed84994d39e5a1702ee0977a17
Marvin Porsil <marvin.porsil@rwth-aachen.de>
2023-05-16 16:53:02 +0200

    Visualization color map legend title.

--- src/org/processmining/OCLPMDiscovery/visualization/components/ColorMapPanel.java
+++ src/org/processmining/OCLPMDiscovery/visualization/components/ColorMapPanel.java
@@ -2,6 +2,7 @@ package org.processmining.OCLPMDiscovery.visualization.components;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.util.HashMap;
@@ -18,16 +19,26 @@ public class ColorMapPanel extends JPanel implements Scrollable {
     private final int rowSeparation = 2;
     private final int diameter = cellHeight-2*rowSeparation;
     private final int textDistance = 6; // additional distance from circles to the string
+    private final Font font = new Font("Arial", Font.PLAIN, 14);
+    private final Font fontBold = new Font("Arial", Font.BOLD, 16);
 
     public ColorMapPanel(HashMap<String, Color> colorMap) {
         this.colorMap = colorMap;
-        setPreferredSize(new Dimension(200, colorMap.size() * cellHeight));    
+        setPreferredSize(new Dimension(200, (colorMap.size()+1) * cellHeight)); // +1 for the legend    
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+//        Font currentFont = g.getFont();
         int i = 0;
+        // paint title
+        g.setColor(Color.BLACK);
+        g.setFont(fontBold);
+        g.drawString("Place Object Type Legend", rowSeparation*2+textDistance, (i + 1) * cellHeight - 10);
+        g.setFont(font);
+        i++;
+        // paint entries
         for (Map.Entry<String, Color> entry : colorMap.entrySet()) {
         	g.setColor(Color.BLACK);
             g.fillOval(rowSeparation, (i * cellHeight)+rowSeparation, diameter, diameter); // circle border
