c8c15fbb0a7718f41414e218ee4eeaad9a409206
Marvin Porsil <marvin.porsil@rwth-aachen.de>
2023-07-06 18:04:47 +0200

    Visualizer: User can select other visualizer for different color theme.

--- src/org/processmining/OCLPMDiscovery/plugins/visualization/OCLPMResultVisualizer.java
+++ src/org/processmining/OCLPMDiscovery/plugins/visualization/OCLPMResultVisualizer.java
@@ -13,14 +13,23 @@ import org.processmining.framework.plugin.annotations.Plugin;
 import org.processmining.framework.plugin.annotations.PluginVariant;
 
 
-@Plugin(name = "@0 Visualize OCLPM Result",
-        returnLabels = {"Visualized OCLPM Result"},
-        returnTypes = {JComponent.class},
-        parameterLabels = {"Petri Net Array"})
-@Visualizer
+//@Plugin(name = "@0 Visualize OCLPM Result",
+//        returnLabels = {"Visualized OCLPM Result"},
+//        returnTypes = {JComponent.class},
+//        parameterLabels = {"Petri Net Array"})
+//@Visualizer
 public class OCLPMResultVisualizer {
+	
+//	public OCLPMColors theme = OCLPMColors.getClownMode();
+//	public OCLPMColors theme = OCLPMColors.getDarkMode();
+	public OCLPMColors theme = OCLPMColors.getLightMode();
 
-    @PluginVariant(requiredParameterLabels = {0})
+	@Plugin(name = "@0 OCLPM Visualizer Light",
+            returnLabels = {"Visualized OCLPM Result"},
+            returnTypes = {JComponent.class},
+            parameterLabels = {"Petri Net Array"})
+    @Visualizer
+    @PluginVariant(variantLabel = "OCLPM Visualizer Light", requiredParameterLabels = {0})
     public JComponent visualize(UIPluginContext context, OCLPMResult result) {
 
         if (result.size() < 1)
@@ -28,13 +37,31 @@ public class OCLPMResultVisualizer {
         
         result.refreshColors();
         
-//        OCLPMColors theme = OCLPMColors.getClownMode();
-//        OCLPMColors theme = OCLPMColors.getDarkMode();
-        OCLPMColors theme = OCLPMColors.getLightMode();
-        
         OCLPMResultPluginVisualizerTableFactory factory = new OCLPMResultPluginVisualizerTableFactory();
         SimpleCollectionOfElementsComponent scoec = new SimpleCollectionOfElementsComponent<>(context, result, factory, theme);
         return scoec;
+    }
+    
+    @Plugin(name = "@1 OCLPM Visualizer Dark",
+            returnLabels = {"Visualized OCLPM Result"},
+            returnTypes = {JComponent.class},
+            parameterLabels = {"Petri Net Array"})
+    @Visualizer
+    @PluginVariant(variantLabel = "OCLPM Visualizer Dark", requiredParameterLabels = {0})
+    public JComponent visualizeDark(UIPluginContext context, OCLPMResult result) {
+    	this.theme = OCLPMColors.getDarkMode();
+    	return visualize(context,result);
+    }
+    
+    @Plugin(name = "OCLPM Visualizer Debug",
+            returnLabels = {"Visualized OCLPM Result"},
+            returnTypes = {JComponent.class},
+            parameterLabels = {"Petri Net Array"})
+    @Visualizer
+    @PluginVariant(variantLabel = "OCLPM Visualizer Clown", requiredParameterLabels = {0})
+    public JComponent visualizeClown(UIPluginContext context, OCLPMResult result) {
+    	this.theme = OCLPMColors.getClownMode();
+    	return visualize(context,result);
     }
 }
 
